378dde22ea04f646c7d9f71c128ab358ead5bee0
Kalyan Vissamsetty <vissamsettykalyan@gmail.com>
2023-11-08 01:34:35 +0530

    Time: 99 ms (89.07%), Space: 89.1 MB (30.13%) - LeetHub

---
+++ 1921-eliminate-maximum-number-of-monsters/1921-eliminate-maximum-number-of-monsters.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int eliminateMaximum(vector<int>& dist, vector<int>& speed) {
+        vector<double> arrival;
+        for (int i = 0; i < dist.size(); i++) {
+            arrival.push_back((double) dist[i] / speed[i]);
+        }
+        sort(arrival.begin(), arrival.end());
+        int ans = 0;
+        for (int i = 0; i < arrival.size(); i++) {
+            if (arrival[i] <= i) {
+                break;
+            }
+            ans++;
+        }
+        return ans;
+    }
+};
