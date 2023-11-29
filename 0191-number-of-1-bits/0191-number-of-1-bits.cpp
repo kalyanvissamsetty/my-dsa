7bc7ea196b9085acf77d606caaaae72698935610
Kalyan Vissamsetty <vissamsettykalyan@gmail.com>
2023-11-29 10:16:21 +0530

    Time: 4 ms (30.61%), Space: 6.4 MB (57.22%) - LeetHub

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        uint32_t bitMask = 1;
+        uint32_t count = 0;
+        while(n)
+        {
+            if(n & bitMask)
+                count++;
+            n = n >> bitMask;
+        }
+        return count;
+    }
+};
